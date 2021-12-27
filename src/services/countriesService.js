b1a81af4464c8c6a5b46314cb49e01bf16aefb85
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-27 05:32:46 -0300

    Edit countries service

--- src/services/countriesService.js
+++ src/services/countriesService.js
@@ -2,14 +2,25 @@ import axios from 'axios';
 
 const API = process.env.REACT_APP_REST_COUNTRIES_URL;
 
-export async function getCountries({ region, name }) {
-  const { data } = await axios.get(
-    region ? `${API}/region/${region}` : name ? `${API}/name/${name}` : `${API}/all`
-  );
+export const REGIONS = [
+  { id: 'africa', value: 'Africa' },
+  { id: 'americas', value: 'America' },
+  { id: 'asia', value: 'Asia' },
+  { id: 'europe', value: 'Europe' },
+  { id: 'oceania', value: 'Oceania' },
+];
+
+export async function getCountries() {
+  const { data } = await axios.get(`${API}/all`);
+  return data;
+}
+
+export async function getCountriesRegion(region) {
+  const { data } = await axios.get(`${API}/region/${region}`);
   return data;
 }
 
 export async function getCountry(name) {
   const { data } = await axios.get(`${API}/name/${name}`);
-  return data;
+  if (data?.length > 0) return data[0];
 }
